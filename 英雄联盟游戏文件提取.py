e8f7410fcb6e5f832982ff89ee333bd07fda33f0
WordlessMeteor <1925009271@qq.com>
2026-09-04 19:12:45 +0800

    日志时间戳精度提高【Precision Improvement of Log Timestamps】
    跟进英雄联盟自制脚本存储库的提交0a97701，现在日志文件中的时间戳服从ISO 6801格式。输出到终端中的时间戳仍然是年月日时分秒的格式，以免时间戳占用过长宽度。
    Following WordlessMeteor/LoL-DIY-Programs@0a97701, timestamps in log files now follow the ISO 6801 format. Timestamps output to terminal are still in "YYYY-MM-DD HH:MM:SS" format, in case they would take up too many characters of width.
    此外，补充了两处变量类型注释。（第8、12和13行。）
    Besides, supplemented two variable type annotations. (Lines 8, 12 and 13.)

--- 英雄联盟游戏文件提取.py
+++ 英雄联盟游戏文件提取.py
@@ -1,36 +1,42 @@
-import cssbeautifier, jsbeautifier, json, os, time, re, shutil, traceback, _io
+import cssbeautifier, datetime, jsbeautifier, json, os, time, re, shutil, traceback, _io
 from humanize import naturalsize
 from cdtb.hashes import default_hashfile, HashFile
 from cdtb.wad import Wad
 from cdtb.binfile import BinFile
 from cdtb.rstfile import RstFile, get_hashfile, key_to_hash
 from cdtb.export import AtlasInfoConverter, ChampionSkinInfoConverter
-from typing import Any, Literal, Optional
+from typing import Any, IO, Literal, Optional
 from bs4 import BeautifulSoup
 
 os.makedirs("Data/Update Logs", exist_ok = True)
-currentTime = time.strftime("%Y-%m-%d %H-%M-%S", time.localtime())
-log = open(f"Data/Update Logs/{currentTime}.log", "w", encoding = "utf-8")
+currentTime: str = time.strftime("%Y-%m-%d %H-%M-%S", time.localtime())
+log: IO[Any] = open(f"Data/Update Logs/{currentTime}.log", "w", encoding = "utf-8")
+
+def getISOTime(timestamp: float) -> str:
+    dt: datetime.datetime = datetime.datetime.fromtimestamp(timestamp, tz = datetime.timezone.utc)
+    return dt.isoformat(timespec = "milliseconds").replace("+00:00", "Z")
 
 def logInput(prompt: str = "", log: _io.TextIOWrapper = log, write_time: bool = True) -> str:
     s = input(prompt)
     if isinstance(log, _io.TextIOWrapper):
         if write_time:
-            currentTime = time.strftime("%Y-%m-%d %H-%M-%S", time.localtime())
-            log.write("[%s]%s\n" %(currentTime, prompt + s))
+            currentTime_millisecond: str = getISOTime(time.time())
+            log.write("[%s]%s\n" %(currentTime_millisecond, prompt + s))
         else:
             log.write(prompt + s + "\n")
     return s
 
 def logPrint(s: object = "", log: _io.TextIOWrapper = log, end: str = "\n", print_time: bool = False, write_time: bool = True) -> None:
-    currentTime = time.strftime("%Y-%m-%d %H-%M-%S", time.localtime())
+    currentTime: float = time.time()
+    currentTime_second: str = time.strftime("%Y-%m-%d %H-%M-%S", time.localtime(currentTime))
+    currentTime_millisecond: str = getISOTime(currentTime)
     if print_time:
-        print("[%s]%s" %(currentTime, s), end = end, flush = end == "\r")
+        print("[%s]%s" %(currentTime_second, s), end = end, flush = end == "\r")
     else:
         print(s, end = end, flush = end == "\r")
     if isinstance(log, _io.TextIOWrapper):
         if write_time:
-            log.write("[%s]%s%s" %(currentTime, str(s), "\n" if end == "\r" else end))
+            log.write("[%s]%s%s" %(currentTime_millisecond, str(s), "\n" if end == "\r" else end))
         else:
             log.write("%s%s" %(str(s), "\n" if end == "\r" else end))
 
